ea0ac07a96d114c8fa673c54e978697c59891ebd
wattai <evamark.wattai@gmail.com>
2021-12-01 03:00:39 +0900

    Update codes

--- src/sample/calculation.py
+++ src/sample/calculation.py
@@ -1,5 +1,41 @@
 # -*- coding: utf-8 -*-
 
+__all__ = ["add", "sub"]
 
-def add(a, b):
+
+def add(a: float, b: float) -> float:
+    """Add two values.
+
+    Args:
+        a (float): 1st argument.
+        b (float): 2nd argument.
+
+    Returns:
+        float: summation.
+
+    Examples:
+        How to use.
+
+        >>> add(1.0, 2.5)
+        3.5
+    """
     return a + b
+
+
+def sub(a: float, b: float) -> float:
+    """Sub two values.
+
+    Args:
+        a (float): 1st argument.
+        b (float): 2nd argument.
+
+    Returns:
+        float: subtract.
+
+    Examples:
+        How to use.
+
+        >>> sub(3.0, 1.2)
+        1.8
+    """
+    return a - b
--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -2,10 +2,16 @@
 
 import pytest
 
-from sample.calculation import add
+from sample.calculation import add, sub
 
 
 @pytest.mark.parametrize("a", [-1, 0, 1])
 @pytest.mark.parametrize("b", [-1, 0, 1])
 def test_add(a, b):
     assert add(a, b) == a + b
+
+
+@pytest.mark.parametrize("a", [-1, 0, 1])
+@pytest.mark.parametrize("b", [-1, 0, 1])
+def test_sub(a, b):
+    assert sub(a, b) == a - b
